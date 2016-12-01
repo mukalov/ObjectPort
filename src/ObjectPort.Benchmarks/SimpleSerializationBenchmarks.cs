a492cfe0f873e2c51eac8c1fd3408922e4a9ba31
Dmitry Mukalov <mukalov@gmail.com>
2016-12-01 22:35:53 +0200

    Extended test data

--- src/ObjectPort.Benchmarks/SimpleSerializationBenchmarks.cs
+++ src/ObjectPort.Benchmarks/SimpleSerializationBenchmarks.cs
@@ -5,6 +5,7 @@
     using BenchmarkDotNet.Attributes.Jobs;
     using Serializers;
     using System;
+    using System.Linq;
 
     [ClrJob, CoreJob]
     [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
@@ -13,11 +14,33 @@
 #if !NETCORE
         [Serializable]
 #endif
+        public class TestClass2
+        {
+            public string Field1 { get; set; }
+            public int Field2 { get; set; }
+            public int Prop1 { get; set; }
+        }
+
+#if !NETCORE
+        [Serializable]
+#endif
+        public class TestClass3
+        {
+            public string Field1 { get; set; }
+            public int Field2 { get; set; }
+        }
+
+#if !NETCORE
+        [Serializable]
+#endif
         public class TestClass
         {
             public string Field1 { get; set; }
             public int Field2 { get; set; }
             public int Prop1 { get; set; }
+            public TestClass2 Prop2 { get; set; }
+
+            public TestClass3[] Prop3 { get; set; }
         }
 
         private TestClass _testObj;
@@ -26,17 +49,25 @@
         public SimpleSerializationBenchmarks()
         {
             _rnd = new Random();
-            Inititalize(new[] { typeof(TestClass) });
+            Inititalize(new[] { typeof(TestClass), typeof(TestClass2), typeof(TestClass3) });
         }
 
         [Setup]
         public void Setup()
         {
+            var strGen = new StringGenerator();
             _testObj = new TestClass
             {
-                Field1 = new StringGenerator().Generate(20, 50),
+                Field1 = strGen.Generate(20, 50),
                 Field2 = _rnd.Next(0, int.MaxValue),
-                Prop1 = _rnd.Next(0, int.MaxValue)
+                Prop1 = _rnd.Next(0, int.MaxValue),
+                Prop2 = new TestClass2
+                {
+                    Field1 = strGen.Generate(20, 50),
+                    Field2 = _rnd.Next(0, int.MaxValue),
+                    Prop1 = _rnd.Next(0, int.MaxValue)
+                },
+                Prop3 = Enumerable.Range(0, 20).Select(i => new TestClass3 { Field1 = strGen.Generate(20, 50), Field2 = i }).ToArray()
             };
             foreach (var serializer in _serializers)
                 serializer.Value.InitializeIteration();
