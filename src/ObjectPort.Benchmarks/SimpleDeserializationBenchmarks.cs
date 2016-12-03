7fccdd61760bf1455c5da561230ab15f4c584960
Dmitry Mukalov <mukalov@gmail.com>
2016-12-03 20:23:15 +0200

    Benchmarks refactoring

--- src/ObjectPort.Benchmarks/SimpleDeserializationBenchmarks.cs
+++ src/ObjectPort.Benchmarks/SimpleDeserializationBenchmarks.cs
@@ -5,38 +5,37 @@
     using BenchmarkDotNet.Attributes.Jobs;
     using Serializers;
     using System;
+    using System.Linq;
 
     [ClrJob, CoreJob]
     [MarkdownExporter, AsciiDocExporter, HtmlExporter, CsvExporter, RPlotExporter]
     public class SimpleDeserializationBenchmarks : SerializationBenchmarks
     {
-#if !NETCORE
-        [Serializable]
-#endif
-        public class TestClass
-        {
-            public string Field1 { get; set; }
-            public int Field2 { get; set; }
-            public int Prop1 { get; set; }
-        }
-
         private TestClass _testObj;
         private Random _rnd;
 
         public SimpleDeserializationBenchmarks()
         {
             _rnd = new Random();
-            Inititalize(new[] { typeof(TestClass) });
+            Inititalize(new[] { typeof(TestClass), typeof(TestClass2), typeof(TestClass3) });
         }
 
         [Setup]
         public void Setup()
         {
+            var strGen = new StringGenerator();
             _testObj = new TestClass
             {
-                Field1 = new StringGenerator().Generate(20, 50),
+                Field1 = strGen.Generate(20, 50),
                 Field2 = _rnd.Next(0, int.MaxValue),
-                Prop1 = _rnd.Next(0, int.MaxValue)
+                Prop1 = _rnd.Next(0, int.MaxValue),
+                Prop2 = new TestClass2
+                {
+                    Field1 = strGen.Generate(20, 50),
+                    Field2 = _rnd.Next(0, int.MaxValue),
+                    Prop1 = _rnd.Next(0, int.MaxValue)
+                },
+                Prop3 = Enumerable.Range(0, 20).Select(i => new TestClass3 { Field1 = strGen.Generate(20, 50), Field2 = i }).ToArray()
             };
             foreach (var serializer in _serializers)
             {
---
+++ src/ObjectPort.Benchmarks/TestData.cs
@@ -0,0 +1,36 @@
+﻿namespace ObjectPort.Benchmarks
+{
+    using System;
+
+#if !NETCORE
+    [Serializable]
+#endif
+    public class TestClass2
+    {
+        public string Field1 { get; set; }
+        public int Field2 { get; set; }
+        public int Prop1 { get; set; }
+    }
+
+#if !NETCORE
+    [Serializable]
+#endif
+    public class TestClass3
+    {
+        public string Field1 { get; set; }
+        public int Field2 { get; set; }
+    }
+
+#if !NETCORE
+    [Serializable]
+#endif
+    public class TestClass
+    {
+        public string Field1 { get; set; }
+        public int Field2 { get; set; }
+        public int Prop1 { get; set; }
+        public TestClass2 Prop2 { get; set; }
+
+        public TestClass3[] Prop3 { get; set; }
+    }
+}
